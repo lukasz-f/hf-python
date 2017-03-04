699e389ca44b0d284037e7685e1908424266bbdf
lukasz f <lukasz@MacBook-lukasz.local>
2017-03-04 13:54:57 +0100

    operacje na listach

---
+++ 01 meet python/cast list.py
@@ -0,0 +1,42 @@
+#Utworzenie listy imion
+cast = ["Cleese", 'Palin', 'Jones', "Idle"]
+
+#Wyświetlenie listy
+print(cast)
+
+#Wyświetlenie długości listy
+print(len(cast))
+
+#Wyświetlenie pojedynczego elementu listy
+print(cast[1])
+
+#Dodanie elementu na koniec listy
+cast.append("Gilliam")
+print(cast)
+
+#Usunięcie ostatniego elementu
+cast.pop()
+print(cast)
+
+#Dodanie kilku elementów na koniec listy
+cast.extend(["Gilliam", "Chapman"])
+print(cast)
+
+#Różnica między ‚append’ a ‚extend’
+cast.append(['a', 'b'])
+print(cast);
+cast.pop();
+
+cast.extend(['a', 'b'])
+print(cast)
+cast.pop()
+cast.pop()
+print(cast)
+
+#Znalezienie i usunięcie elementu
+cast.remove("Chapman")
+print(cast)
+
+#Dodanie elementu przed podanym indeksem
+cast.insert(0, "Chapman")
+print(cast)
---
+++ 01 meet python/movie list.py
@@ -0,0 +1,10 @@
+#Lista filmów
+movies = ["the Holy Grail", "The Life of Brian", "The Meaning of Life"]
+
+#Wstawienie daty nakręcenia filmów
+movies.insert(1, 1975)
+movies.insert(3, 1979)
+movies.append(1983)
+print(movies)
+
+#Iteracja po liście
