5493e8d7cb9688ffa18bd74a730dd42886e3dcf8
lukasz f <lukaszf6@gmail.com>
2017-03-04 20:09:55 +0100

    dodanie bifsów (built-in functions)

---
+++ 01 meet python/bifsy.py
@@ -0,0 +1,14 @@
+#BIFs = built-in function
+
+#Wyświetlanie na ekranie
+print('asdf')
+
+#Obliczenie wielkości listy
+cast = ["Cleese", 'Palin', 'Jones', "Idle"]
+len(cast)
+
+#Sprawdzenie czy zmienna jest listą
+isinstance(cast, list)
+num_names = len(cast)
+isinstance(num_names, list)
+
