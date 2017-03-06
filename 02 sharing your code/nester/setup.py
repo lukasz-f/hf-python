732bc2b9030e1828d4fe6dd7c2dcc84b12a82586
lukasz f <lukaszf6@gmail.com>
2017-03-06 20:47:34 +0100

    przygotowanie plików do wydanie dystrybucji

---
+++ 02 sharing your code/nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester',
+	version = '1.0.0',
+	py_modules = ['nester'],
+	author = 'lsf',
+	author_email = 'lsfapps@gmail.com',
+	url = 'http://www.google.com',
+	description = 'A simple printer of nested lists.'
+)
