3c20493a6596e628a71997ba4f7110350fb26220
lukasz f <lukaszf6@gmail.com>
2017-03-06 20:45:29 +0100

    poprawki w komentarzach

--- 02 sharing your code/comments.py
+++ 02 sharing your code/comments.py
@@ -1,8 +1,10 @@
 """This is module 
 multi-line comment"""
+
 def methd_name(parameters):
-	"""This is comment
-	for function"""
+	'''This is comment
+	for function'''
+
 	print("inside method")
 
 	#Single-line comment
