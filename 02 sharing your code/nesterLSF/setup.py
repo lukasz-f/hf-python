b6d492005c64f11eb42dc7257f31da492262875c
lukasz f <lukaszf6@gmail.com>
2017-03-08 21:20:54 +0100

    podbicie wersji

--- 02 sharing your code/nesterLSF/setup.py
+++ 02 sharing your code/nesterLSF/setup.py
@@ -2,10 +2,6 @@ from distutils.core import setup
 
 setup(
 	name = 'nesterLSF',
-	version = '1.0.0',
-	py_modules = ['nesterLSF'],
-	author = 'lsf',
-	author_email = 'lsfapps@gmail.com',
-	url = 'http://www.google.com',
-	description = 'A simple printer of nested lists.'
+	version = '1.0.1',
+	py_modules = ['nesterLSF']
 )
