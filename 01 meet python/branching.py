6a1524529d4a2fd099fd0b7a3538006863a3b0d1
lukasz f <lukaszf6@gmail.com>
2017-03-08 18:02:09 +0100

    zmiany w instrukcjach warunkowych

---
+++ 01 meet python/branching.py
@@ -0,0 +1,9 @@
+#Instrukcja if else
+if 34 > 43:
+	print('34')
+elif 43 == 34:
+	print('4334')
+else:
+	print('43')
+
+#Brak instrukcji switch
--- 01 meet python/if_else.py
+++
@@ -1,5 +0,0 @@
-#Instrukcja if else
-if 43 > 34:
-	print('43')
-else:
-	print('34')
