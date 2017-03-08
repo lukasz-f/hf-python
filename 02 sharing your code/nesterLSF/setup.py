aa902e4663932baf65b338d3f3428469a03b7ce5
lukasz f <lukaszf6@gmail.com>
2017-03-08 21:20:26 +0100

    refaktoring

--- 02 sharing your code/nesterLSF/setup.py
+++ 02 sharing your code/nesterLSF/setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-	name = 'nester',
+	name = 'nesterLSF',
 	version = '1.0.0',
-	py_modules = ['nester'],
+	py_modules = ['nesterLSF'],
 	author = 'lsf',
 	author_email = 'lsfapps@gmail.com',
 	url = 'http://www.google.com',
