d10be0db95e1add7d3216b0a43f32b22905b979a
lukasz f <lukasz@MacBook-lukasz.local>
2017-03-09 14:30:40 +0100

    dodanie obsługi możliwości braku pliku

--- 03 files and exceptions/sketch1.py
+++ 03 files and exceptions/sketch1.py
@@ -2,17 +2,17 @@
 import os
 
 os.chdir('/Users/lukasz/hf-python/03 files and exceptions')
-data = open('sketch.txt')
-print(data.readline())
+if os.path.exists('sketch.txt'): # Check if file exists
+	data = open('sketch.txt')
 
-data.seek(0)
+	#Display on screen every line of file
+	for each_line in data:
+		if not each_line.find(':') == -1: #Check if line contains a colon
+			(role, line_spoken) = each_line.split(':', 1)
+			print(role, end='')
+			print(' said: ', end='')
+			print(line_spoken, end='')
 
-#Display on screen every line of file
-for each_line in data:
-	if not each_line.find(':') == -1:
-		(role, line_spoken) = each_line.split(':', 1)
-		print(role, end='')
-		print(' said: ', end='')
-		print(line_spoken, end='')
-
-data.close()
+	data.close()
+else:
+	print('The data file is missing!')
--- 03 files and exceptions/sketch2.py
+++ 03 files and exceptions/sketch2.py
@@ -2,19 +2,19 @@
 import os
 
 os.chdir('/Users/lukasz/hf-python/03 files and exceptions')
-data = open('sketch.txt')
-print(data.readline())
+try:
+	data = open('sketch.txt')
 
-data.seek(0)
+	#Display on screen every line of file
+	for each_line in data:
+		try:
+			(role, line_spoken) = each_line.split(':', 1)
+			print(role, end='')
+			print(' said: ', end='')
+			print(line_spoken, end='')
+		except ValueError:
+			pass
 
-#Display on screen every line of file
-for each_line in data:
-	try:
-		(role, line_spoken) = each_line.split(':', 1)
-		print(role, end='')
-		print(' said: ', end='')
-		print(line_spoken, end='')
-	except:
-		pass
-
-data.close()
+	data.close()
+except IOError:
+	print('The data file is missing!')
