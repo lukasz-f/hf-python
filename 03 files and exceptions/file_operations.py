a00695dc567067be34530fc2e558742d1090fb94
lukasz f <lukasz@MacBook-lukasz.local>
2017-03-09 14:31:12 +0100

    operacje na plikach

---
+++ 03 files and exceptions/file_operations.py
@@ -0,0 +1,7 @@
+data = open('sketch.txt') #Open file
+
+data.readline() #Use readline to grab a line from file
+
+data.seek(0) #Return to the start of the file
+
+data.close() #You are done so close file
