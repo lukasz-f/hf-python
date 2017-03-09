06350a2f784aded421be1d8643dfc1c897bf2b3f
lukasz f <lukasz@MacBook-lukasz.local>
2017-03-09 14:32:32 +0100

    os std lib

---
+++ 03 files and exceptions/os_std_lib.py
@@ -0,0 +1,7 @@
+#os from Standard Library
+import os
+
+os.getcwd() #returns current working directory
+os.chdir('/Users/lukasz/hf-python/03 files and exceptions') #change directory
+
+os.path.exists('sketch.txt') #Check whether the file exists
