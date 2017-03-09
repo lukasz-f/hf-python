bf823fce8e7d351208e12819df3672a106866bbf
lukasz f <lukasz@MacBook-lukasz.local>
2017-03-09 12:45:54 +0100

    kolejne bifsy

--- 02 sharing your code/bifsy.py
+++ 02 sharing your code/bifsy.py
@@ -4,6 +4,7 @@ range() 	#Returns an iterator that generates numbers in a specified range on dem
 range(10) 			#0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 range(1, 11) 		#1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 range(10, 0, -1) 	#10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+list(range(4))
 
 enumerate() #Creates a numbered list of paired-data, starting from 0
 
