13cb7627fd74fba217d2f5a2067c26bf03a2ee04
lukasz f <lukaszf6@gmail.com>
2017-03-10 19:43:45 +0100

    dodanie nowych operacji na pliku

--- 03 files and exceptions/file_operations.py
+++ 03 files and exceptions/file_operations.py
@@ -1,7 +1,28 @@
 data = open('sketch.txt') #Open file
+data = open('sketch.txt', 'r') #Open file for reading
+
+out = open('data.out', 'w') #Open file for writing
 
 data.readline() #Use readline to grab a line from file
+
+print('asdf', file=out) #Use print to write to file
 
 data.seek(0) #Return to the start of the file
 
 data.close() #You are done so close file
+
+#try/except/finally
+try:
+	file = open('file.txt')
+except IOError as err:
+	print('File error: ' + str(err))
+finally:
+	if 'file' in locals(): #Check if file object was created
+		file.close()
+
+#try with/except
+try:
+	with open('file.txt') as file:
+		print("It's...", file=file)
+except IOError as err:
+	print('File error: ' + str(err))
