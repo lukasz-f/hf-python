01f2175bc8fbefd9cbb7d38e64705c0b84195340
lukasz f <lukaszf6@gmail.com>
2017-03-10 19:24:35 +0100

    dodanie finally do try

--- 03 files and exceptions/exceptions_handling.py
+++ 03 files and exceptions/exceptions_handling.py
@@ -13,4 +13,18 @@ except:
 try:
 	#your code which might cause a runtime error
 except ValueError:
-	print('ValueError occure')
+	print('ValueError occurs')
+
+#Handle with exception object
+try:
+	#your code which might cause a runtime error
+except ValueError as err:
+	print('ValueError occurs: ' + str(err))
+
+#Handle exception if occur and always run finally code
+try:
+	#your code which might cause a runtime error
+except IOError:
+	#your code when exception occurs
+finally:
+	#your code that always run
