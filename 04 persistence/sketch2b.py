26882e0fd8029000bc55a8ce429ca9ca42f3e5e0
lukasz f <lukaszf6@gmail.com>
2017-03-10 19:44:31 +0100

    zapis danych do pliku, wersja try with/except

---
+++ 04 persistence/sketch2b.py
@@ -0,0 +1,25 @@
+#sketch try with/except version
+man = []
+other = []
+
+try:
+	with open('sketch.txt') as data:
+		for each_line in data:
+			try:
+				(role, line_spoken) = each_line.split(':', 1)
+				line_spoken = line_spoken.strip()
+				if role == 'Man':
+					man.append(line_spoken)
+				elif role == 'Other Man':
+					other.append(line_spoken)
+			except ValueError:
+				pass
+except IOError:
+	print('The datafile is missing!')
+
+try:
+	with open('man_data.txt', 'w') as man_file, open('other_data.txt', 'w') as other_file:
+		print(man, file=man_file)
+		print(other, file=other_file)
+except IOError as err:
+	print('File error: ' + str(err))
