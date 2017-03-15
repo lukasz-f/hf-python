e96d515cf71d21cab8f40d3b7ef0e983a3cdd550
lukasz f <lukaszf6@gmail.com>
2017-03-15 16:10:53 +0100

    Athelets module with sorted and sanitized lists with list comprehension

---
+++ 05 comprehending data/athletes3.py
@@ -0,0 +1,43 @@
+'''Athelets module with sorted and sanitized lists with list comprehension'''
+
+
+def sanitize(time_sting):
+    if '-' in time_sting:
+        splitter = '-'
+    elif ':' in time_sting:
+        splitter = ':'
+    else:
+        return time_sting
+
+    (mins, secs) = time_sting.split(splitter)
+    return mins + '.' + secs
+
+
+james = []
+julie = []
+mikey = []
+sarah = []
+
+try:
+    with open('james.txt') as james_file:
+        data = james_file.readline()
+    james = data.strip().split(',')
+
+    with open('julie.txt') as julie_file:
+        data = julie_file.readline()
+    julie = data.strip().split(",")
+
+    with open('mikey.txt') as mikey_file:
+        data = mikey_file.readline()
+    mikey = data.strip().split(',')
+
+    with open('sarah.txt') as sarah_file:
+        data = sarah_file.readline()
+    sarah = data.strip().split(',')
+except IOError as err:
+    print('File error: ' + str(err))
+
+print(sorted([sanitize(each_time) for each_time in james]))
+print(sorted([sanitize(each_time) for each_time in julie]))
+print(sorted([sanitize(each_time) for each_time in mikey]))
+print(sorted([sanitize(each_time) for each_time in sarah]))
