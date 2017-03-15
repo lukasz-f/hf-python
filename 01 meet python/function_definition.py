dcb7205127583cacde783a582bc6cf6cef5081d3
lukasz f <lukaszf6@gmail.com>
2017-03-15 21:13:52 +0100

    function returns None

--- 01 meet python/function_definition.py
+++ 01 meet python/function_definition.py
@@ -11,3 +11,18 @@ movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
           ["Graham Chapman", ["Michael Palin", "John Cleese", "Terry Gilliam",
                               "Eric Idle", "Terry Jones"]]]
 print_lol(movies)
+
+
+# Function returns None
+def fun1():
+    return None
+
+
+# Function returns None
+def fun2():
+    return
+
+
+# Function returns None
+def fun3():
+    pass
