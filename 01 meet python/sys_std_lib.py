9d0170234133d27e40331dcb7b52c405c52a7903
lukasz f <lukaszf6@gmail.com>
2017-03-19 18:05:14 +0100

    sys std lib

---
+++ 01 meet python/sys_std_lib.py
@@ -0,0 +1,11 @@
+# sys from Standard Library
+import sys
+
+# ilość parametrów przekazanych przy uruchomieniu skryptu
+print(len(sys.argv))
+
+# wyświetla nazwe skryptu
+print(sys.argv[0])
+
+# wyświetla pierwszy parametr
+print(sys.argv[1])
