317a078f08167b98a03bcf221277e7e6b5900c7c
lukasz f <lukasz@MacBook-lukasz.local>
2017-03-26 20:03:24 +0200

    del bif

--- 04 persistence/bifsy.py
+++ 04 persistence/bifsy.py
@@ -1,1 +1,13 @@
 locals()  # Zwraca słownik z nazwami i wartościami lokalnych zmiennych
+
+# Remove definition of variable.
+# Delete part of the list and entries from dictionaries.
+del()
+var = 6
+del(var)  # var no more!
+
+list = ['a', 'b', 'c', 'd']
+del(list[0])  # Delete first element
+
+dict = {'a': 1, 'b': 2, 'c': 3}
+del(dict['b'])  # Delete 'b' entry
