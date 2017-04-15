c9261fda80568f6677a6a380d2459ea9b7adf0f7
lukasz f <lukasz@MacBook-lukasz.local>
2017-04-15 22:11:21 +0200

    new list operations

--- 01 meet python/list_operations.py
+++ 01 meet python/list_operations.py
@@ -14,44 +14,48 @@ print(cast[1])
 print(cast[0:3])
 
 # Nie tworzy kopii. Obie zmienne wskazują na tą samą listę
-b = cast
+a = cast
 
 # Tworzy kopie listy
 b = cast[:]
+c = list(cast)
 
 # Dodanie elementu na koniec listy
-cast.append("Gilliam")
+cast += ['Gilliam']
+cast.append('Gilliam')
+print(cast)
+
+# Dodanie kilku elementów na koniec listy
+cast += ["Gilliam", "Chapman"]
+cast.extend(["Gilliam", "Chapman"])
+print(cast)
+
+# Różnica między 'append' a 'extend'
+lista = [0]
+lista.append(['a', 'b'])
+lista.extend(['c', 'd'])
+print(lista)
+
+# Dodanie elementu przed podanym indeksem
+cast.insert(0, "Chapman")
 print(cast)
 
 # Usunięcie ostatniego elementu
 cast.pop()
-print(cast)
-
-# Dodanie kilku elementów na koniec listy
-cast.extend(["Gilliam", "Chapman"])
-print(cast)
-
-# Różnica między ‚append’ a ‚extend’
-cast.append(['a', 'b'])
-print(cast)
-cast.pop()
-
-cast.extend(['a', 'b'])
-print(cast)
-cast.pop()
-cast.pop()
+del(cast[-1])
 print(cast)
 
 # Removes and returns the element at the given index. Returns the rightmost
 # element if index is omitted (roughly the opposite of append())
 cast.pop(0)
+print(cast)
 
-# Znalezienie i usunięcie elementu
+# Znalezienie i usunięcie pierwszego odnalezionego elementu
 cast.remove("Chapman")
 print(cast)
 
-# Dodanie elementu przed podanym indeksem
-cast.insert(0, "Chapman")
+# Usuniecie kilku elementow
+del(cast[-2:])
 print(cast)
 
 # In-place sorting. Arrange in the order and replace original data
@@ -83,12 +87,17 @@ print([each_name.upper() for each_name in cast])
 # index. Throws a ValueError if the element does not appear.
 cast.index('Palin')
 
+[1, 2, 3, 4, 1, 5, 6, 1].count(1)  # Number of number 1 occurrences: 3
+
 # Reverses the list in place (does not return it)
 cast.reverse()
 
 # Slices
-list = ['a', 'b', 'c', 'd']
-list[1:-1]  # ['b', 'c']
-list[::2]  # ['a', 'c']
-list[0:2] = 'z'  # replace ['a', 'b'] with ['z']
+letters = ['a', 'b', 'c', 'd']
+letters[1:-1]  # ['b', 'c']
+letters[::2]  # ['a', 'c']
 
+# Changing list elements
+letters[0] = 'A'  # ['A', 'b', 'c', 'd']
+letters[2:4] = ['d', 'c']  # ['A', 'b', d', 'c']
+letters[2:4] = 'z'  # ['A', 'b', 'z']
