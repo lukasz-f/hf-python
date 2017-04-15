284f5654cfaeabe47fdc4cbfc67222aed7a2fcf2
lukasz f <lukasz@MacBook-lukasz.local>
2017-04-15 22:12:09 +0200

    bifsy: bool, complex, max, round

--- 02 sharing your code/bifsy.py
+++ 02 sharing your code/bifsy.py
@@ -1,19 +1,23 @@
 # Returns an iterator that generates numbers in a specified range on demand
 # and as needed
-range()
 range(10)  # 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 range(1, 11)  # 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 range(10, 0, -1)  # 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 
-enumerate()  # Creates a numbered list of paired-data, starting from 0
+# Creates a numbered list of paired-data, starting from 0
+enumerate(['a', 'c', 'd'])
 for i, element in enumerate([5, 2, 8, 0]):
     print(i, element)
 
-int()  # Converts a strings or another number to an integer (if possible)
+int()  # Factory function that creates a integer with value 0
+int('1')  # Converts a strings or another number to an integer (if possible)
+int(True)  # 1
 
-str()  # Converts values to a string (if possible)
+str()  # Factory function that creates a new, empty string
+str(1)  # Converts values to a string (if possible)
+str(True)  # 'True'
 
-id()  # Returns the unique identification for a Python data object
+id(None)  # Returns the unique identification for a Python data object
 str = 'abc'
 id(str)  # 4320935640
 str += 'd'  # string jest immutable
@@ -23,6 +27,15 @@ next()  # Returns the next item form a iterable data structure such as a list
 
 float()  # Converts to floating point
 float('2.22')
+
+bool()  # Factory function that creates a boolean with value False
+bool(2)  # Coverts int to boolean True
+bool(0)  # False
+bool('')  # False
+bool('a')  # True
+
+complex(1)  # 1+0j
+complex(1, 1) # 1+1j
 
 type(1)  # Zwraca typ zmiennej
 a = 'a'
@@ -47,3 +60,8 @@ tuple()
 t1 = ((),)  # Utworzenie pustej krotki
 t2 = (1,)  # Utworzenie jednoelementowej krotki
 t3 = (1, 2)  # Utworzenie dwuelementowej krotki
+
+max([1, 11, 111])  # 111
+
+round(2.111)  # 2
+round(2.111, 1)  # 2.1
